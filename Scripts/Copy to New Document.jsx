27851b58a4a6dff8922bffe2417d53bdcba4850a
ashung <ashung.hung@gmail.com>
2013-03-18 11:25:08 +0800

    Add more scripts

---
+++ Scripts/Copy to New Document.jsx
@@ -0,0 +1,43 @@
+﻿// Copy to New Document
+// by Ashung Hung
+
+try {
+    cTID = function(s) { return app.charIDToTypeID(s); };
+    sTID = function(s) { return app.stringIDToTypeID(s); };
+    
+    // Make     
+    var desc1 = new ActionDescriptor();
+    var ref1 = new ActionReference();
+        ref1.putClass(cTID('Dcmn'));
+        desc1.putReference(cTID('null'), ref1);
+    var ref2 = new ActionReference();
+        ref2.putEnumerated(cTID('Lyr '), cTID('Ordn'), cTID('Trgt'));
+        desc1.putReference(cTID('Usng'), ref2);
+        desc1.putInteger(cTID('Vrsn'), 5);
+    executeAction(cTID('Mk  '), desc1, DialogModes.NO);
+    
+    // Image - Trim
+    var desc2 = new ActionDescriptor();
+        desc2.putEnumerated(sTID("trimBasedOn"), sTID("trimBasedOn"), cTID('Trns'));
+        desc2.putBoolean(cTID('Top '), true);
+        desc2.putBoolean(cTID('Btom'), true);
+        desc2.putBoolean(cTID('Left'), true);
+        desc2.putBoolean(cTID('Rght'), true);
+    executeAction(sTID('trim'), desc2, DialogModes.NO);
+    
+    // View - Actual Pixels
+    var desc3 = new ActionDescriptor();
+    var ref3 = new ActionReference();
+        ref3.putEnumerated(cTID('Mn  '), cTID('MnIt'), cTID('ActP'));
+        desc3.putReference(cTID('null'), ref3);
+    executeAction(cTID("slct"), desc3, DialogModes.NO);
+    
+    // View - Show - None
+    var desc4 = new ActionDescriptor();
+    var ref4 = new ActionReference();
+        ref4.putEnumerated(cTID('Mn  '), cTID('MnIt'), sTID('showNone'));
+        desc4.putReference(cTID('null'), ref4);
+    executeAction(cTID("slct"), desc4, DialogModes.NO);
+   
+} catch (e) { }
+
---
+++ Scripts/Make Normal Image for Retina Image.jsx
@@ -0,0 +1,13 @@
+﻿// Make Normal Image for Retina Image
+// by Ashung Hung
+
+try {
+    var desc1 = new ActionDescriptor();
+        desc1.putUnitDouble(charIDToTypeID('Wdth'), charIDToTypeID('#Prc'), 50);
+        desc1.putBoolean(stringIDToTypeID("scaleStyles"), true);
+        desc1.putBoolean(charIDToTypeID('CnsP'), true);
+        desc1.putEnumerated(charIDToTypeID('Intr'), charIDToTypeID('Intp'), stringIDToTypeID('bicubicSharper'));
+    executeAction(stringIDToTypeID('imageSize'), desc1, DialogModes.NO);
+} catch (e) {
+   alert(e.message);
+}
