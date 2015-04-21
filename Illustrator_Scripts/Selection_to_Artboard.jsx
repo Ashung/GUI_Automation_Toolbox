6db34de18e4f0c90d6a834a4a163424e049cc43d
Ashung Hung <Ashung.hung@gmail.com>
2015-04-21 21:41:55 +0800

    Fix Artboard_Export.jsx bug.

--- Illustrator_Scripts/Selection_to_Artboard.jsx
+++ Illustrator_Scripts/Selection_to_Artboard.jsx
@@ -1,14 +1,1 @@
-
-
-
-var selectedObjects = activeDocument.selection;
-
-for(var i = 0; i < selectedObjects.length; i ++) {
-    var rect = [];
-    for(var j = 0; j < selectedObjects[i].controlBounds.length; j ++) {
-        rect.push(Math.round(selectedObjects[i].controlBounds[j]));
-    }
-    //$.writeln(rect);
-    activeDocument.artboards.add(rect);
-    selectedObjects[i].remove();
-}
+﻿/* * Copyright (c) 2015 Ashung Hung (Ashung.hung@gmail.com) *  * Licensed under the Apache License, Version 2.0 (the "License"); * you may not use this file except in compliance with the License. * You may obtain a copy of the License at * *    http://www.apache.org/licenses/LICENSE-2.0 * * Unless required by applicable law or agreed to in writing, software * distributed under the License is distributed on an "AS IS" BASIS, * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. * See the License for the specific language governing permissions and * limitations under the License. *  * * ----- * Convert Selections to Artboards for Adobe Illustrator * Project Home: github.com/Ashung/GUI_Automation_Toolbox * ----- */(function(){    'use strict'        var selectedObjects = activeDocument.selection;    for(var i = 0; i < selectedObjects.length; i ++) {        var rect = [];        for(var j = 0; j < selectedObjects[i].controlBounds.length; j ++) {            rect.push(Math.round(selectedObjects[i].controlBounds[j]));        }        //$.writeln(rect);                if(activeDocument.artboards.length >= 100) {            alert("Adobe Illustrator only support 100 artboards.");            break;        }                activeDocument.artboards.add(rect);        selectedObjects[i].remove();    }})();
